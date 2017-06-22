fb73e321790d1640afb0ca9aaa16f683368bbe42
Paulina Gonzalez C <pgc.lacroix@gmail.com>
2017-06-22 09:43:50 -0400

    se agregan funcionalidades y se arreglan problemas

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,79 @@
 document.getElementById("btn-menu").addEventListener("click", function() {
     document.getElementById("despliega").classList.toggle("hamburguesa");
 });
+
+function initMap(){
+   	var map = new google.maps.Map(document.getElementById("mapa"),{
+		zoom: 5,
+		center: {lat: -33.43778, lng: -70.65028},
+		mapTypeControl: false,
+		zoomControl: true,
+		streetViewControl: true,
+	});
+
+	function buscar(){
+		if(navigator.geolocation){
+			navigator.geolocation.getCurrentPosition(funcionExito, funcionError);
+		}
+	}
+
+	window.addEventListener("load", buscar)
+	var latitud, longitud;
+
+	var funcionExito = function(posicion){
+		latitud = posicion.coords.latitude;
+		longitud = posicion.coords.longitude;
+
+		var miUbicacion = new google.maps.Marker({
+			position: {lat:latitud, lng:longitud},
+			animation: google.maps.Animation.DROP,
+			map: map,
+		});
+
+		map.setZoom(17);
+		map.setCenter({lat:latitud, lng:longitud});
+
+	}
+
+	var funcionError = function(error){
+		alert("Tenemos un problema con encontrar tu ubicación.");
+	}
+
+	//Autocompletar//
+	var inputInicio = (document.getElementById("inicio"));
+	var autocompletar = new google.maps.places.Autocomplete(inputInicio);
+        autocompletar.bindTo("bounds", map);
+
+    var inputLlegada = (document.getElementById("destino"));
+	var autocompletar = new google.maps.places.Autocomplete(inputLlegada);
+        autocompletar.bindTo("bounds", map);
+
+	var directService = new google.maps.DirectionsService;
+    var directDisplay = new google.maps.DirectionsRenderer;
+    	
+	document.getElementById("inicio").addEventListener("change", cambios);
+	document.getElementById("destino").addEventListener("change", cambios);
+
+
+	function trazarRuta(directService, directDisplay) {
+        directService.route({
+        	origin: document.getElementById("inicio").value,
+	        destination: document.getElementById("destino").value,
+	        travelMode: "DRIVING"
+	        }, 
+	    function(response, status) {
+	     	if (status === "OK") {
+	        	directDisplay.setDirections(response);
+	      	} else {
+	            window.alert("No se encontró la ruta :( " + status);
+	        }
+	    });
+	}	
+	directDisplay.setMap(map);
+	
+	var cambios = function(){
+		trazarRuta(directService, directDisplay);
+	};  
+
+	document.getElementById("ruta").addEventListener("click", cambios); 
+}
